1d3a70319135ba62e7ab7e56dfabf62f2d260882
HIREN lalani <72781029+hiren14@users.noreply.github.com>
2021-07-18 16:25:45 +0530

    Create Patten_2.c

---
+++ Patten_2.c
@@ -0,0 +1,50 @@
+#include<stdio.h> /*
+1 12
+123 1234 12345 [Proc */
+int main() {
+int i,j;
+for(i=i;i<=5;i++) {
+for(j=1;j<=i;j++) {
+printf("%d",j); }
+printf("\n"); }
+//printf("Hello world!"); return 0;
+}
+------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------//2
+#include<stdio.h> /*
+54321 4321 321 21 1
+[Proc [Proc */
+int main() {int n;
+printf("ENTER THE LENGTH OF PATTERN"); int i,j;
+scanf("%d",&n); for(i=n;i>=1;i--) {
+for(j=i;j>=1;j--) {
+printf("%d",j); }
+printf("\n"); }
+//printf("Hello world!"); return 0;}
+
+------------------------------------------------------------------------------------------------------------------------------------------------
+//3 
+#include<stdio.h> /*
+ENTER THE LENGTH OF PATTERN 5 55555 4444 333 22 1
+[Proc */int main() {
+int n; printf("ENTER THE LENGTH OF PATTERN ");
+scanf("%d",&n); int i,j;
+for(i=n;i>=1;i--) {
+for(j=i;j>=1;j--) {
+printf("%d",i); }
+printf("\n"); }
+//printf("Hello world!"); return 0;
+}
+------------------------------------------------------------------------------------------------------------------------------------------------
+//iv
+#include<stdio.h> /*
+1 22
+333 444455555
+[Proc */
+int main() {
+int i,j;
+for(i=i;i<=5;i++) {
+for(j=1;j<=i;j++) {
+printf("%d",i); }
+printf("\n"); }
+return 0; 
