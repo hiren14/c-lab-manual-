12bbabe5235a529fef270f7fd1c0d2d676b4924d
HIREN lalani <72781029+hiren14@users.noreply.github.com>
2021-07-18 16:01:13 +0530

    Create Num_greatest_3.c

---
+++ Num_greatest_3.c
@@ -0,0 +1,20 @@
+/*Aim : Write a program to read three numbers from keyboard and find out maximum out of these three. (nested if else).
+//file name --- num_gerater-3.c*/
+#include <stdio.h>
+int main() {
+    double n1, n2, n3;
+    printf("Enter three different numbers: ");
+    scanf("%lf %lf %lf", &n1, &n2, &n3);
+    // if n1 is greater than both n2 and n3, n1 is the largest
+    if (n1 >= n2 && n1 >= n3)
+        printf("%.2f is the largest number.", n1);
+
+    // if n2 is greater than both n1 and n3, n2 is the largest
+    if (n2 >= n1 && n2 >= n3)
+        printf("%.2f is the largest number.", n2);
+
+    // if n3 is greater than both n1 and n2, n3 is the largest
+    if (n3 >= n1 && n3 >= n2)
+        printf("%.2f is the largest number.", n3);
+
+    return 0;}
