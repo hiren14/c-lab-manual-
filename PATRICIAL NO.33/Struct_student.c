4608880a7eb1db54e864b1e1d02601beb743bada
HIREN lalani <72781029+hiren14@users.noreply.github.com>
2021-07-19 14:44:49 +0530

    Create Struct_student.c

---
+++ PATRICIAL NO.33/Struct_student.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+//Design a structure  to contain name,
+//branch and total marks obtained.
+// Develop a program to read data for 10 students
+// in a class and print them.
+struct student_record
+{
+    char name[50];
+    char branch[50];
+    int total;
+}p[10];
+int main()
+{
+    int n=10;
+    int i=0;
+    for(i=0; i<n; i++)
+    {
+        printf("ENTER THE NAME ");
+        scanf("%s",&p[i].name);
+        printf("ENTER THE BRANCH ");
+        scanf("%s",&p[i].branch);
+        printf("ENTER THE TOTAL_MARKS ");
+        scanf("%s",&p[i].total);
+    }
+//printing
+
+    for(i=0; i<n; i++)
+    {
+        printf("STUDENTS DETAILS %d",i+1);
+        printf("\n NAME %s",p[i].name);
+        printf("\n BRANCH %d ",p[i].branch);
+        printf("\n total %d",p[i].total);
+    }
+
+    //printf("Hello world!");
+    return 0;
+}
